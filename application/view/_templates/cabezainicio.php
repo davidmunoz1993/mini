f9ceef0da982c3249fedca3b581a92707236df62
davidmunoz1993 <juliandmarias@gmail.com>
2019-03-19 21:41:27 -0500

    inicio de repo

---
+++ application/view/_templates/cabezainicio.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.8">
+    <meta http-equiv="x-UA-compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/inicio.css">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <link rel="icon" href="img/logo.jpeg">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    <title>Egresados </title>
+</head>
+<body class="body">
+   
+    <div  class="subirdiv" >
+        <nav class="navbar navbar-expand-lg navbar-light color2div">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+                <a style="color: white" class="navbar-brand colorletra"  href="#" >EGRESOFT</a>
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        
+                            <a title="" href=""><h4 class=""></h4></a>
+                        
+                    
+        </nav>
+</div>
+</body>
+</html>

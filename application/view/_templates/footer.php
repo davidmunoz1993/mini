fdf2f9c65144f12d2256af1051c8bccb969923d1
davidmunoz1993 <juliandmarias@gmail.com>
2019-03-19 21:56:47 -0500

    modificacion de footer

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -1,7 +1,7 @@
 
     <div class="footer">
         Find <a href="https://github.com/panique/mini3">MINI3 on GitHub</a>.
-        If you like the project, support it by <a href="http://tracking.rackspace.com/SH1ES">using Rackspace</a> as your hoster [affiliate link].
+        If you like the project, support it by una prueba<a href="http://tracking.rackspace.com/SH1ES">using Rackspace</a> as your hoster [affiliate link].
     </div>
 
     <!-- jQuery, loaded in the recommended protocol-less way -->
